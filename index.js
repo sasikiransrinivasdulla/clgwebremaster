22fea4bedf0df4636f9b17c2580224da9dfbb78c
SYED UMAR <umar630934@gmail.com>
2024-05-28 12:57:47 +0530

    feat: Added the cet codes for large navbar

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.querySelector('.navbar');
     const navbar2 = document.querySelector('.navbar-expand-lg');
     const navbar2ndLarge = document.querySelector('#second-large-navbar');
+    const codeNavbar = document.getElementById('codeNavbar');
     const nav2text = document.getElementById("nav-bar2-text");
     const nav2text2 = document.getElementById("nav-bar2-text2");
     const nav2text3= document.getElementById("nav-bar2-text3");
@@ -73,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav2text6= document.getElementById("nav-bar2-text6");
     const nav2text7= document.getElementById("nav-bar2-text7");
     const nav2text8= document.getElementById("nav-bar2-text8");
+    const codeTest = document.getElementById("codeTest");
     const dropdownTextElementUGC=document.getElementById("dropdownTextElementUGC");
     const dropdownTextElementNIRF=document.getElementById("dropdownTextElementNIRF");
     const showDiv = document.querySelector('.nav-color');
@@ -98,6 +100,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const navelement=document.getElementById('second-large-navbar');
             navbar.style.backgroundColor = '#b1040e'; 
             navbar2ndLarge.style.backgroundColor='white';
+            codeNavbar.style.backgroundColor='white';
             nav2text.classList.remove("hover-underline-animation");
             nav2text.classList.add("hover-underline-animation-1");
             nav2text.style.color="black";
@@ -121,6 +124,9 @@ document.addEventListener('DOMContentLoaded', function () {
             dropdownTextElementUGC.setAttribute('style', 'color: black !important;font-weight:500;border-style:none;');
             nav2text8.classList.remove("hover-underline-animation");
             nav2text8.classList.add("hover-underline-animation-1");
+            codeTest.classList.remove("hover-underline-animation");
+            codeTest.classList.add("hover-underline-animation-1");
+            codeTest.style.color="black";
             dropdownTextElementNIRF.setAttribute('style', 'color: black !important;font-weight:500;border-style:none;');
             navelement.classList.add("shadow-lg");
             hasVisitedShowDiv = false; 
@@ -172,12 +178,14 @@ function scrollFunction() {
     const navbar = document.querySelector('.navbar');
     const navbar2 = document.querySelector('.navbar-expand-lg');
     const navbar2ndLarge = document.querySelector('#second-large-navbar');
+    const codeNavbar = document.getElementById('codeNavbar');
     const nav2text = document.getElementById("nav-bar2-text");
     const nav2text2 = document.getElementById("nav-bar2-text2");
     const nav2text3= document.getElementById("nav-bar2-text3");
     const nav2text4 = document.getElementById("nav-bar2-text4");
     const nav2text5= document.getElementById("nav-bar2-text5");
     const nav2text6= document.getElementById("nav-bar2-text6");
+    const codeTest = document.getElementById("codeTest");
 
     const nav2text8= document.getElementById("nav-bar2-text8");
     const dropdownTextElementUGC=document.getElementById("dropdownTextElementUGC");
@@ -187,6 +195,7 @@ function scrollFunction() {
     navbar.style.backgroundColor = "transparent";
     navbar2.style.backgroundColor = "transparent";
     navbar2ndLarge.style.backgroundColor="transparent";
+    codeNavbar.style.backgroundColor="transparent";
     nav2text.classList.remove("hover-underline-animation-1");
     nav2text.classList.add("hover-underline-animation");
     nav2text.style.color="white";
@@ -207,6 +216,9 @@ function scrollFunction() {
     nav2text6.style.color="white";
     nav2text7.classList.remove("hover-underline-animation-1");
     nav2text7.classList.add("hover-underline-animation");
+    codeTest.classList.remove("hover-underline-animation-1");
+    codeTest.classList.add("hover-underline-animation");
+    codeTest.style.color="white";
     dropdownTextElementUGC.setAttribute('style', 'color: white !important;font-weight:500;border-style:none;');
     nav2text8.classList.remove("hover-underline-animation-1");
     nav2text8.classList.add("hover-underline-animation");
